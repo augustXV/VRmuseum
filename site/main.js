c1dc32dbb7cd2e7d3a6f6e0041f198e06f6b4735
august <DonHock@yandex.ru>
2022-10-20 11:37:01 +0300

    changed menu button and background

--- site/main.js
+++ site/main.js
@@ -1,11 +1,24 @@
 const navP = document.querySelectorAll(".nav_p")
 const navTarget = document.querySelectorAll(".nav_target")
+const menuBtn = document.querySelector(".menu_btn")
+const headerPicList = document.querySelector(".header_pic_list")
+
+menuBtn.addEventListener("click", function() {
+    if (!headerPicList.classList.contains("header_pop_up")){
+        headerPicList.classList.remove("header_pic_list");
+        headerPicList.classList.add("header_pop_up");
+
+    } else if (headerPicList.classList.contains("header_pop_up")) {
+        headerPicList.classList.remove("header_pop_up");
+        headerPicList.classList.add("header_pic_list");
+    }
+})
 
 for(let item of navP) {
     item.addEventListener("click", function() {
         for(let targetItem of navTarget){
             if (item.textContent == targetItem.textContent){
-                targetItem.scrollIntoView({block: "end", behavior: "smooth"});
+                targetItem.scrollIntoView({block: "center", behavior: "smooth"});
             }
         }
     })
